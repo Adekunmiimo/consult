3a52570efc50932ead49e1e92a1cb5a2f2cd1f0a
Adekunmi <adekunmiimooluwa@gmail.com>
2024-07-26 11:29:46 +0100

    .

---
+++ teks/page5.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+        strings: ["Fullstack Developer", "Data Scientist"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
